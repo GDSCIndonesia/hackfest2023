ffa48761fff280fad211f47358420c51090da85d
NicoAL95 <nicoabellaia@gmail.com>
2023-01-26 11:28:18 +0700

    Finish coming soon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,30 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins, sans-serif',
+        product: 'Product Sans, sans-serif',
+      },
+      animation: {
+        colorAni: 'colorAni 5s ease infinite',
+      },
+      keyframes: {
+        colorAni: {
+          '0%, 100%': {
+            'background-size': '150% 150%',
+            'background-position': 'left right',
+          },
+          '50%': {
+            'background-size': '150% 150%',
+            'background-position': 'right top',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
