cff7e41f646ed2a7f70930172861a6aeac93af96
NicoAL95 <nicoabellaia@gmail.com>
2023-02-04 22:40:22 +0700

    Move | Finish Header Title - Navbar - Whats Is GSC - You All Know

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import ComingSoon from './components/ComingSoon'
+import HackfestDetail from './components/HackfestDetail'
 
-ComingSoon
+import HackTitle from './components/HackTitle'
+import Navbar from './components/layout/Navbar'
+import TopWords from './components/TopWords'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ComingSoon/>
+    <>
+      <HackTitle/>
+      <Navbar/>
+      <HackfestDetail/>
+      <TopWords/>
+    </>
   )
 }
 
