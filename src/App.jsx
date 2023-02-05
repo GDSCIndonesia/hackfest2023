250c832633ca3d38091859ad9e904335dc733032
NicoAL95 <nicoabellaia@gmail.com>
2023-02-06 01:19:07 +0700

    Nico first week checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
+import CountDown from './components/CountDown'
 import HackfestDetail from './components/HackfestDetail'
 
 import HackTitle from './components/HackTitle'
+import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import MediaPartner from './components/MediaPartner'
+import OrganizerEvent from './components/OrganizerEvent'
+import SponsorEvent from './components/SponsorEvent'
 import TopWords from './components/TopWords'
 
 function App() {
@@ -14,6 +19,11 @@ function App() {
       <Navbar/>
       <HackfestDetail/>
       <TopWords/>
+      <CountDown/>
+      <MediaPartner/>
+      <SponsorEvent/>
+      <OrganizerEvent/>
+      <Footer/>
     </>
   )
 }
