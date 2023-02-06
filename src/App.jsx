a385231a47c098418270f4b32f537ebaa0298cda
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2023-02-06 19:06:11 +0700

    fix: minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import HackfestDetail from './components/HackfestDetail'
-
 import HackTitle from './components/HackTitle'
-import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
-import MediaPartner from './components/MediaPartner'
-import OrganizerEvent from './components/OrganizerEvent'
-import SponsorEvent from './components/SponsorEvent'
+import HackfestDetail from './components/HackfestDetail'
 import TopWords from './components/TopWords'
+import CountEvent from './components/CountEvent'
 import Timeline from './components/Timeline'
 import Speaker from './components/Speaker'
 import Mentor from './components/Mentor'
+import MediaPartner from './components/MediaPartner'
+import SponsorEvent from './components/SponsorEvent'
 import Faq from './components/Faq'
-import CountEvent from './components/CountEvent'
 import JoinDiscord from './components/JoinDiscord'
+import OrganizerEvent from './components/OrganizerEvent'
+import Footer from './components/layout/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <HackTitle/>
@@ -28,7 +24,6 @@ function App() {
       <Timeline/>
       {/* <Speaker/> */}
       {/* <Mentor/> */}
-      <CountEvent/>
       {/* <MediaPartner/> */}
       {/* <SponsorEvent/> */}
       <Faq/>
--- src/components/JoinDiscord.jsx
+++ src/components/JoinDiscord.jsx
@@ -11,7 +11,7 @@ export default function JoinDiscord() {
                 <div>
                     <h1 className="text-white font-gooBold text-[35px] sm:text-[50px] md:text-[35px] lg:text-[50px] xl:text-[60px] leading-tight">Join Our</h1>
                     <h1 className="text-white font-gooBold text-[35px] sm:text-[50px] md:text-[35px] lg:text-[50px] xl:text-[60px] leading-tight">Discord Channel</h1>
-                    <p className="text-white font-gooReg text-[13px] md:text-[12px] lg:text-[14px] mt-[10px]">Get all the information about HackFest 2023 through the official GDSC Indonesia discord channel. Don't miss it, make sure you join by pressing the button below</p>
+                    <p className="text-white font-gooReg text-[13px] md:text-[12px] lg:text-[14px] mt-[10px]">Get all the information about HackFest 2023 through the official Google Developers Indonesia discord channel. Don't miss it, make sure you join by pressing the button below.</p>
                     <a
                     href="https://goo.gle/googledevsid"
                     className="inline-block bg-transparent text-white font-gooReg py-[10px] md:py-[15px] px-[30px] md:px-[40px] hover:bg-white hover:text-black transition-all w-full lg:w-fit text-center border-2 border-white mt-[30px] xl:mt-[40px] text-[16px] lg:text-[18px]"
