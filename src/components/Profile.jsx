e60642876b215bf9a9b05c11b93b12d1cb851441
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2023-02-06 12:22:21 +0700

    fix: profile margin and sizing

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 export default function Profile({ name, position, image }) {
   return (
-    <div className="border border-black text-center bg-white">
-      <div className="flex aspect-1 justify-center items-center">
-        <div className="bg-gooBlue-500 aspect-1 h-56 rounded-full border border-black">
+    <div className="border border-black text-center bg-white p-4">
+      <div className="flex justify-center items-center">
+        <div className="bg-gooBlue-500 w-full h-full aspect-1 m-12 xl:m-12 lg:m-8 rounded-full border border-black">
           <img src={image} /*alt={name + " profile"}*/ />
         </div>
       </div>
