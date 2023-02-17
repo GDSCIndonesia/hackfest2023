d88bcd97bea8a0e3a29f552d7d5c3fdba204b27f
NicoAL95 <nicoabellaia@gmail.com>
2023-02-17 22:01:19 +0700

    Update alert

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,12 @@ import Faq from './components/Faq'
 import JoinDiscord from './components/JoinDiscord'
 import OrganizerEvent from './components/OrganizerEvent'
 import Footer from './components/layout/Footer'
+import AlertHead from './components/AlertHead'
 
 function App() {
   return (
     <>
+      <AlertHead/>
       <HackTitle/>
       <Navbar/>
       <HackfestDetail/>
