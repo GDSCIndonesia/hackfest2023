10abc792de8c438633c297e83f67a4cc9926500d
NicoAL95 <nicoabellaia@gmail.com>
2023-02-18 10:03:00 +0700

    Update alert description

--- src/components/AlertHead.jsx
+++ src/components/AlertHead.jsx
@@ -7,7 +7,7 @@ export default function AlertHead() {
   return (
     <section className={`${showAlert === true ? "" : "hidden"} bg-black p-8`}>
         <div className="flex justify-between items-start space-x-3 sm:space-x-4">
-            <h1 className='text-white font-gooReg text-[13px] md:text-[15px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, rerum libero repudiandae, qui ipsam consequuntur veritatis repellat labore molestias illum quod minus sit doloremque sed aliquam ut et veniam fugit!</h1>
+            <h1 className='text-white font-gooReg text-[13px] md:text-[15px]'>The registration will be closed on Sunday, 19th February 2023 at 12.00 pm GMT+7 (12.00 WIB)</h1>
             <button onClick={() => setAlert(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
