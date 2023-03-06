7e0051ce47acca7edeac9e4a389d84cf00998fdb
NicoAL95 <nicoabellaia@gmail.com>
2023-03-06 23:33:27 +0700

    Restructure HTML and resize image

--- src/components/MediaPartner.jsx
+++ src/components/MediaPartner.jsx
@@ -8,29 +8,29 @@ export default function MediaPartner() {
           <div className="w-full h-full bg-media-lines bg-cover bg-repeat-y bg-top py-[30px] lg:py-[60px] px-[30px] lg:px-[60px]">
             <div className="text-center md:text-left">
               <h1 className="font-gooBold text-black text-[50px] md:text-[65px] tracking-[-.10rem] md:tracking-[-.20rem]">Media Partner</h1>
-              <h2 className="font-gooReg text-black text-[14px] tracking-[-.001rem] sm:text-[16px]">HackFest 2023 Indonesia Official Media Partner</h2>
+              <h2 className="font-gooReg text-black text-[14px] tracking-[-.001rem] sm:text-[16px]">Google HackFest 2023 Indonesia Official Media Partner</h2>
             </div>
-            <div className="flex flex-col md:flex-row space-x-[10px] sm:space-x-[20px] justify-center md:justify-evenly my-[30px] flex-wrap space-y-6 md:space-y-0">
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/1.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+            <div className="grid grid-cols-4 gap-8 mt-[30px]">
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/1.png" className="w-full h-auto" alt=""/>
               </div>
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/2.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/2.png" className="w-full h-auto" alt=""/>
               </div>
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/3.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/3.png" className="w-full h-auto" alt=""/>
               </div>
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/4.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/4.png" className="w-full h-auto" alt=""/>
               </div>
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/5.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/5.png" className="w-full h-auto" alt=""/>
               </div>
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/6.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/6.png" className="w-full h-auto" alt=""/>
               </div>
-              <div className="flex items-center justify-center">
-                <img src="/libraries/images/media-partner/7.png" className="w-[180px] md:w-[120px] h-auto" alt=""/>
+              <div className="col-span-2 sm:col-span-1 flex items-center">
+                <img src="/libraries/images/media-partner/7.png" className="w-full h-auto" alt=""/>
               </div>
             </div>
           </div>
